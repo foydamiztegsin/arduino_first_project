8cad9b7505b31d9f5518f5fa1d1e828a4aa05b84
foydamiztegsin <121927950+foydamiztegsin@users.noreply.github.com>
2023-04-12 13:43:24 +0500

    Create arduno.py

---
+++ arduno.py
@@ -0,0 +1,47 @@
+import pyfirmata
+
+board=pyfirmata.Arduino('COM3')
+
+led_1=board.get_pin('d:13:o')
+led_2=board.get_pin('d:12:o')
+led_3=board.get_pin('d:11:o')
+led_4=board.get_pin('d:10:o')
+led_5=board.get_pin('d:9:o')
+
+def led(raqam):
+    if raqam==0:
+        led_1.write(0)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+    elif raqam==1:
+        led_1.write(1)
+        led_2.write(0)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+    elif raqam==2:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(0)
+        led_4.write(0)
+        led_5.write(0)
+    elif raqam==3:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(0)
+        led_5.write(0)
+    elif raqam==4:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        led_5.write(0)
+    elif raqam==5:
+        led_1.write(1)
+        led_2.write(1)
+        led_3.write(1)
+        led_4.write(1)
+        led_5.write(1)
